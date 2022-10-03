269ad64435c9a7aa901e8ba26a55fa90a7655d05
Michael Gallaspy <mgallaspy@cloudera.com>
2022-10-03 14:33:03 -0700

    Update gen & disc architecture
    Update the architecture to reflect a reference implementation.
    Some details differ compared to what's described in the paper.

--- scripts/cyclegan_models.py
+++ scripts/cyclegan_models.py
@@ -17,10 +17,12 @@ class ResidualBlock(torch.nn.Module):
         super(ResidualBlock, self).__init__()
 
         self.model = torch.nn.Sequential(
+            torch.nn.ReflectionPad2d(padding=1),
             torch.nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, stride=1, padding=0, bias=False),
             torch.nn.InstanceNorm2d(num_features=num_features),
             torch.nn.ReLU(),
 
+            torch.nn.ReflectionPad2d(padding=1),
             torch.nn.Conv2d(in_channels=num_features, out_channels=num_features, kernel_size=3, stride=1, padding=0, bias=False),
             torch.nn.InstanceNorm2d(num_features=num_features),
         )
@@ -38,13 +40,8 @@ class GeneratorModel(torch.nn.Module):
         super(GeneratorModel, self).__init__()
 
         self.model = torch.nn.Sequential(
-            torchvision.transforms.Pad(padding=40, padding_mode='reflect'),
-
-            torch.nn.Conv2d(in_channels=cin, out_channels=32, kernel_size=9, stride=1, padding=4, bias=False),
-            torch.nn.InstanceNorm2d(num_features=32),
-            torch.nn.ReLU(),
-
-            torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=1, bias=False),
+            torch.nn.ReflectionPad2d(padding=3),
+            torch.nn.Conv2d(in_channels=cin, out_channels=64, kernel_size=7, stride=1, padding=0, bias=False),
             torch.nn.InstanceNorm2d(num_features=64),
             torch.nn.ReLU(),
 
@@ -52,34 +49,40 @@ class GeneratorModel(torch.nn.Module):
             torch.nn.InstanceNorm2d(num_features=128),
             torch.nn.ReLU(),
 
-            ResidualBlock(num_features=128),
-            ResidualBlock(num_features=128),
-            ResidualBlock(num_features=128),
-            ResidualBlock(num_features=128),
-            ResidualBlock(num_features=128),
+            torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1, bias=False),
+            torch.nn.InstanceNorm2d(num_features=256),
+            torch.nn.ReLU(),
+
+            ResidualBlock(num_features=256),
+            ResidualBlock(num_features=256),
+            ResidualBlock(num_features=256),
+
+            ResidualBlock(num_features=256),
+            ResidualBlock(num_features=256),
+            ResidualBlock(num_features=256),
+
+            ResidualBlock(num_features=256),
+            ResidualBlock(num_features=256),
+            ResidualBlock(num_features=256),
+
+            torch.nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=2, padding=1,
+                                     output_padding=1, bias=False),
+            torch.nn.InstanceNorm2d(num_features=128),
+            torch.nn.ReLU(),
 
             torch.nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1,
                                      output_padding=1, bias=False),
             torch.nn.InstanceNorm2d(num_features=64),
             torch.nn.ReLU(),
 
-            torch.nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3, stride=2, padding=1,
-                                     output_padding=1, bias=False),
-            torch.nn.InstanceNorm2d(num_features=32),
-            torch.nn.ReLU(),
-
-            torch.nn.ConvTranspose2d(in_channels=32, out_channels=cin, kernel_size=9, stride=1, padding=4,
-                                     output_padding=0, bias=False),
-            torch.nn.InstanceNorm2d(num_features=cin),
+            torch.nn.ReflectionPad2d(padding=3),
+            torch.nn.Conv2d(in_channels=64, out_channels=cin, kernel_size=7, stride=1, padding=0, bias=False),
             torch.nn.Tanh(),
         )
 
-        self.register_buffer('c_output_offset', torch.tensor([1]))
-        self.register_buffer('c_output_scale', torch.tensor([0.5]))
-
     def forward(self, input):
         log.debug(f'Input shape={input.shape}')
-        return (self.model(input) + self.c_output_offset) * self.c_output_scale
+        return self.model(input)
 
 
 class PatchGANDiscriminator(torch.nn.Module):
@@ -90,7 +93,7 @@ class PatchGANDiscriminator(torch.nn.Module):
 
         self.model = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels=cin, out_channels=64, kernel_size=4, stride=2, padding=1, bias=False),
-            torch.nn.InstanceNorm2d(num_features=64),
+            torch.nn.LeakyReLU(negative_slope=0.2),
 
             torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=2, padding=1, bias=False),
             torch.nn.InstanceNorm2d(num_features=128),
